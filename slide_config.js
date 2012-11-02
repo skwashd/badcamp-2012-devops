f55a735c7f8b4a3eec625a3566e9f1c08e75f080
Dave Hall <dave.hall@skwashd.com>
2012-11-02 03:57:32 -0700

    First cut

---
+++ slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Enterprise Drupal DevOps<br>Without the Devs or the Ops',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Dave Hall',
+    company: 'Principal Engineer, Technocrat',
+    gplus: 'http://gplus.to/skwashd',
+    twitter: '@skwashd',
+    www: 'http://www.davehall.com.au',
+    github: 'http://github.com/skwashd'
+  }]
+};
+
